7bce180c85e81aae977fd57972ee749b8157b30a
inari_senpai <onemorecoffeepls@gmail.com>
2023-04-06 13:22:37 +0200

    Add project files.

---
+++ TestArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace TestArray
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var min = GetMin(new int[] { -5000, -5, 15 });
+            Console.WriteLine(min);
+            Console.ReadKey();
+        }
+        static int? GetMin(int[] arr)
+        {
+            if (arr is null || arr.Length == 0) { return null; }
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                    min = arr[i];
+            }
+            return min;
+        }
+    }
+}
